ea70ae58d5b29f23a0546f9d67cd81643d760a7d
Aaron1375 <tiron7401@gmail.com>
2023-01-23 14:46:05 +0700

    Community between inventory-service and order-service, working on the bug skuCodes

--- inventory-serive/src/main/java/com/programingtechie/inventoryservice/controller/InventoryController.java
+++ inventory-serive/src/main/java/com/programingtechie/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.programingtechie.inventoryservice.controller;
 
 import com.programingtechie.inventoryservice.service.InventoryService;
+import dto.InventoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,11 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    //http://localhost:8083/api/inventory/iphone13,iphone13-red
+    //http://localhost:8083/api/inventory?skuCode=iphone13&skuCode=iphone13-red
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- order-service/src/main/java/com/programingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programingtechie/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.programingtechie.orderservice.service;
 
+import com.programingtechie.orderservice.dto.InventoryResponse;
 import com.programingtechie.orderservice.dto.OrderLineItemsDto;
 import com.programingtechie.orderservice.dto.OrderRequest;
 import com.programingtechie.orderservice.model.Order;
@@ -9,7 +10,9 @@ import com.programingtechie.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,8 +22,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -31,7 +35,31 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call inventory Service, and place order if producsdasda is in
+        // Stock
+        InventoryResponse[] inventoryResponsesArr = webClient.get()
+                .uri("http://localhost:8083/api/inventory?",
+//                .uri("http://localhost:8083/api/inventory?skuCode=iphone_13_red&skuCode=iphone_13")
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsesArr)
+                .allMatch(InventoryResponse::isInStock);
+
+        System.out.println(skuCodes);
+        System.out.println(allProductsInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
